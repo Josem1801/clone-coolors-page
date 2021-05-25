e843c2fb45bc611e74bf7e683e840b4b92eff612
Jose Manuel Rosales <josemanuelrosales2@gmail.com>
2021-05-25 08:34:00 -0500

    Responsive finish

--- src/components/TemplateHeaderFooter.js
+++ src/components/TemplateHeaderFooter.js
@@ -1,24 +1,32 @@
 import React from 'react'
 import Logo from '../assets/logo.svg'
 import coolorsiOS from '../assets/coolors_ios.png'
-import './navbar.css'
+import './templateHeaderFooter.css'
 import {IoIosArrowDown,
         IoLogoTwitter, 
         IoLogoFacebook, 
         IoLogoPinterest, 
-        IoLogoInstagram
+        IoLogoInstagram, 
 } from 'react-icons/io'
+import {IoMenu} from 'react-icons/io5'
 import {FaProductHunt} from 'react-icons/fa'
+import { useState } from 'react'
 
 
 function TemplateHeaderFooter({children}) {
+  const [menuActive, setMenuActive] = useState(false)
   return (  
     <>
       <header className="header">
+        
+        {/*Hamburger*/}
+        <div className="header__menu-hamburger">
+          <IoMenu className='hamburber'/>
+        </div>
+        
         <a className="header__logo" href='/'>
           <img src={Logo} alt="coolors" className='header__logo-img'/>
         </a>
-        
         <a href="https://apps.apple.com/app/coolors/id956480678" className="header__ad">
           <img src={coolorsiOS} alt="Coolors for iOS" className='header__ad-img' />
           <div className="header__ad-text">
@@ -30,12 +38,12 @@ function TemplateHeaderFooter({children}) {
         
         <nav className='header__nav'>
           <ul>
-            <li><a href="#">Generate</a></li>
-            <li><a href="#">Explore</a></li>
-            <li><a href="#"  className='header__nav-more'>More<IoIosArrowDown className='more'/></a></li>
+            <li><a href="#" className='header__nav-generate'>Generate</a></li>
+            <li><a href="#" className='header__nav-explore'>Explore</a></li>
+            <li><a href="#" className='header__nav-more'>More<IoIosArrowDown className='more'/></a></li>
             <hr size='30'/>
-            <li><a href="#">Sign in </a></li>
-            <li><a href="#" className='header__nav-signup'>Sign up </a></li>
+            <li><a href="#" className='header__nav-signin'>Sign in </a></li>
+            <li><a href="#" className='header__nav-signup'>Sign up</a></li>
           </ul>
         </nav>
       </header>
