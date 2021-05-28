a3c7af87d269aefc798cbbe0c987a4ec0e4f760d
Jose Manuel Rosales <josemanuelrosales2@gmail.com>
2021-05-28 16:30:12 -0500

    fix responsive bug with max-width (feedback)

--- src/App.js
+++ src/App.js
@@ -39,136 +39,138 @@ function App() {
   }, [color1])
 
   return (
-    <TemplateHeaderFooter>
+    <div className="App">
+      <TemplateHeaderFooter>
 
-      <div className="hero">
-        <div className="hero__left">
-          <h1 className='hero__left-title'>The super fast color schemes generator!</h1>
-          <p className='hero__left-description'>Create the perfect palette or get inspired by thousands of beautiful colors schemes.</p>
-          <a href="#" target="_blank" className='hero__left-btn blue'>
-            Start the generator!
-          </a>
-          <a href="#" className="hero__left-btn  white">
-            Explore trending palettes
-          </a>
-        </div>
-        <div className="hero__right">
-            <img src={Image} alt="" className='hero__right-computers'/>
-          <div className="hero__right-boxPalettes">
-            <img src={Palettes} alt="" className='hero__right-palettes'/>
+        <div className="hero">
+          <div className="hero__left">
+            <h1 className='hero__left-title'>The super fast color schemes generator!</h1>
+            <p className='hero__left-description'>Create the perfect palette or get inspired by thousands of beautiful colors schemes.</p>
+            <a href="#" target="_blank" className='hero__left-btn blue'>
+              Start the generator!
+            </a>
+            <a href="#" className="hero__left-btn  white">
+              Explore trending palettes
+            </a>
           </div>
-          <div className="hero__right-colors">
-            <div style={{background: `rgb(${color1},${color2},${color3})`}}></div>
-            <div style={{background: `rgb(${color2},${color3},${color1})`}}></div>
-            <div style={{background: `rgb(${color3},${color1},${color2})`}}></div>
-            <div style={{background: `rgb(${color1},${color3},${color2})`}}></div>
-            <div style={{background: `rgb(${color2},${color1},${color3})`}}></div>
+          <div className="hero__right">
+              <img src={Image} alt="" className='hero__right-computers'/>
+            <div className="hero__right-boxPalettes">
+              <img src={Palettes} alt="" className='hero__right-palettes'/>
+            </div>
+            <div className="hero__right-colors">
+              <div style={{background: `rgb(${color1},${color2},${color3})`}}></div>
+              <div style={{background: `rgb(${color2},${color3},${color1})`}}></div>
+              <div style={{background: `rgb(${color3},${color1},${color2})`}}></div>
+              <div style={{background: `rgb(${color1},${color3},${color2})`}}></div>
+              <div style={{background: `rgb(${color2},${color1},${color3})`}}></div>
+            </div>
           </div>
         </div>
-      </div>
-        
+          
 
-      <div className="products">
-        <ProductBox
-          link='#'
-          Logo={IoIosDesktop}
-          title='Website'
-          description='All the power of Coolors on your computer'
-          bgColor='#DAFFCC'
-          fontColor='#30B300'
-          textHover='Use now for free'
-        />
-        <ProductBox
-          link='#'
-          Logo={AiFillApple}
-          title='iOS App'
-          description='Create, browser and save palettes on the go'
-          bgColor='#CCF2FF'
-          fontColor='#0086B3'
-          textHover='View on the App Store'
-        />
-        <ProductBox
-          link='#'
-          Logo={FaAdobe}
-          title='Adobe Extension'
-          description='All palettes right into your workspace.'
-          bgColor='#E4D2F9'
-          fontColor='#6520BA'
-          textHover='Learn more'
-        />
-        <ProductBox
-          link='#'
-          Logo={IoLogoChrome}
-          title='Chrome Extension'
-          description='Get and edit a paletter every new tab.'
-          bgColor='#FCCFE9'
-          fontColor='#C20A75'
-          textHover='Add to Chrome'
-        />
-        <ProductBox
-          link='#'
-          Logo={IoLogoInstagram}
-          title='Instagram'
-          description='Get your daily inspiration of beautiful paletter.'
-          bgColor='#FBF5D0'
-          fontColor='#D5B810'
-          textHover='Follow us'
-        />
-      </div>
+        <div className="products">
+          <ProductBox
+            link='#'
+            Logo={IoIosDesktop}
+            title='Website'
+            description='All the power of Coolors on your computer'
+            bgColor='#DAFFCC'
+            fontColor='#30B300'
+            textHover='Use now for free'
+          />
+          <ProductBox
+            link='#'
+            Logo={AiFillApple}
+            title='iOS App'
+            description='Create, browser and save palettes on the go'
+            bgColor='#CCF2FF'
+            fontColor='#0086B3'
+            textHover='View on the App Store'
+          />
+          <ProductBox
+            link='#'
+            Logo={FaAdobe}
+            title='Adobe Extension'
+            description='All palettes right into your workspace.'
+            bgColor='#E4D2F9'
+            fontColor='#6520BA'
+            textHover='Learn more'
+          />
+          <ProductBox
+            link='#'
+            Logo={IoLogoChrome}
+            title='Chrome Extension'
+            description='Get and edit a paletter every new tab.'
+            bgColor='#FCCFE9'
+            fontColor='#C20A75'
+            textHover='Add to Chrome'
+          />
+          <ProductBox
+            link='#'
+            Logo={IoLogoInstagram}
+            title='Instagram'
+            description='Get your daily inspiration of beautiful paletter.'
+            bgColor='#FBF5D0'
+            fontColor='#D5B810'
+            textHover='Follow us'
+          />
+        </div>
 
-      <div className="companies">
-        <p className="companies__title">Used by awesome companies</p>
-        <ul className="companies__logos">
-          <li><a href="#"><img src={Airbnb} alt="" /></a></li>
-          <li><a href="#"><img src={Apple} alt="" /></a></li>
-          <li><a href="#"><img src={Disney} alt="" /></a></li>
-          <li><a href="#"><img src={DreamWorks} alt="" /></a></li>
-          <li><a href="#"><img src={Dropbox} alt="" /></a></li>
-          <li><a href="#"><img src={EA} alt="" /></a></li>
-          <li><a href="#"><img src={Hasbro} alt="" /></a></li>
-          <li><a href="#"><img src={Microsoft} alt="" /></a></li>
-          <li><a href="#"><img src={Netflix} alt="" /></a></li>
-          <li><a href="#"><img src={Ubisoft} alt="" /></a></li>
-          <li><a href="#"><img src={WarnerBros} alt="" /></a></li>
-        </ul>
-      </div>
+        <div className="companies">
+          <p className="companies__title">Used by awesome companies</p>
+          <ul className="companies__logos">
+            <li><a href="#"><img src={Airbnb} alt="" /></a></li>
+            <li><a href="#"><img src={Apple} alt="" /></a></li>
+            <li><a href="#"><img src={Disney} alt="" /></a></li>
+            <li><a href="#"><img src={DreamWorks} alt="" /></a></li>
+            <li><a href="#"><img src={Dropbox} alt="" /></a></li>
+            <li><a href="#"><img src={EA} alt="" /></a></li>
+            <li><a href="#"><img src={Hasbro} alt="" /></a></li>
+            <li><a href="#"><img src={Microsoft} alt="" /></a></li>
+            <li><a href="#"><img src={Netflix} alt="" /></a></li>
+            <li><a href="#"><img src={Ubisoft} alt="" /></a></li>
+            <li><a href="#"><img src={WarnerBros} alt="" /></a></li>
+          </ul>
+        </div>
 
-      <div className="information">
-        <div className="information__column">
-            <p className="information__colum-title">Website</p>
-            <a href="#">Generete your palettes</a>
-            <a href="#">Explorer popular paletes</a>
-            <a href="#">Color picker</a>
-            <a href="#">Pick paletter from photo</a>
-            <a href="#">Create a collage</a>
-            <a href="#">Create a gradient palette</a>
-            <a href="#">Create a gradient</a>
-            <a href="#">Explore gradients</a>
-            <a href="#">Contrast checker</a>
+        <div className="information">
+          <div className="information__column">
+              <p className="information__colum-title">Website</p>
+              <a href="#">Generete your palettes</a>
+              <a href="#">Explorer popular paletes</a>
+              <a href="#">Color picker</a>
+              <a href="#">Pick paletter from photo</a>
+              <a href="#">Create a collage</a>
+              <a href="#">Create a gradient palette</a>
+              <a href="#">Create a gradient</a>
+              <a href="#">Explore gradients</a>
+              <a href="#">Contrast checker</a>
+          </div>
+          <div className="information__column">
+            <p className="information__colum-title">Products</p>
+            <a href="#">iOS app</a>
+            <a href="#">Adobe addon</a>
+            <a href="#">Chrome extension</a>
+          </div>
+          <div className="information__column">
+            <p className="information__colum-title">Company</p>
+            <a href="#">Terms of service</a>
+            <a href="#">Privacy policy</a>
+            <a href="#">Cookie policy</a>
+            <a href="#">Help center</a>
+            <a href="#">Advertise</a>
+            <a href="#">Contact</a>
+          </div>
+          <div className="information__column">
+            <p className="information__colum-title">Account</p>
+            <a href="#">Sign in</a>
+            <a href="#">Sign up</a>
+            <a href="#">Reset password</a>
+          </div>
         </div>
-        <div className="information__column">
-          <p className="information__colum-title">Products</p>
-          <a href="#">iOS app</a>
-          <a href="#">Adobe addon</a>
-          <a href="#">Chrome extension</a>
-        </div>
-        <div className="information__column">
-          <p className="information__colum-title">Company</p>
-          <a href="#">Terms of service</a>
-          <a href="#">Privacy policy</a>
-          <a href="#">Cookie policy</a>
-          <a href="#">Help center</a>
-          <a href="#">Advertise</a>
-          <a href="#">Contact</a>
-        </div>
-        <div className="information__column">
-          <p className="information__colum-title">Account</p>
-          <a href="#">Sign in</a>
-          <a href="#">Sign up</a>
-          <a href="#">Reset password</a>
-        </div>
-      </div>
-    </TemplateHeaderFooter>
+        </TemplateHeaderFooter>
+    </div>
   );
 }
 
